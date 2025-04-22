064ae745289fbb9df9e9953c92c7fafe2822c990
Maximiliano Reyes López <113549509+mmaaxi@users.noreply.github.com>
2025-04-22 00:38:31 -0600

    agregando src/test/java/pages/1Page.java

---
+++ src/test/java/pages/1Page.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class 1Page {
+
+    WebDriver driver;
+
+    public 1Page(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void performAction1() {
+        // Code to perform Accion 1
+    }
+
+    public void verifyResultado1() {
+        // Code to verify Resultado 1
+    }
+}
